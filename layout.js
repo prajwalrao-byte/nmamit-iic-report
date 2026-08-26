f34e2d05189fab314f2b65328b4211b08f6e04df
prajwalrao-byte <prajwal.rao@nitte.edu.in>
2026-08-26 21:24:28 +0530

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"NMAMIT IIC Report Portal",description:"IIC Activity Report Management System"};
+export default function RootLayout({children}){return <html><body>{children}</body></html>}
